50e65fbf82003ffc298be6d6b76edade725b1f58
ryanwilliams <r.d.williams.ca@gmail.com>
2025-03-10 21:01:31 -0700

    Initial Commit

---
+++ TYOATests/TYOATests.swift
@@ -0,0 +1,17 @@
+//
+//  TYOATests.swift
+//  TYOATests
+//
+//  Created by Ryan Williams on 2025-03-10.
+//
+
+import Testing
+@testable import TYOA
+
+struct TYOATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
